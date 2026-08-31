8f79e4d6c204c9d3911199fdd25abf1392a3e0d1
saurabh9835 <saurabh983538@gmail.com>
2026-08-31 14:08:03 +0530

    created json for prompt

--- 2.Langchain_prompt/prompt_UI.py
+++ 2.Langchain_prompt/prompt_UI.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
 
@@ -13,24 +13,7 @@ style_input = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "Te
 
 length_input = st.selectbox("Select Explanation length",["Short (1-2 paragraphs)","Medium (3-5 paragraphs)","Large (detailed explanation)"])
 
-#Prompt Template
-
-template = PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input','length_input'],
-validate_template=True
-)
+template = load_prompt('package.json')
 
 #Fill the placeholders
 prompt = template.invoke({
